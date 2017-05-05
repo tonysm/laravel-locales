4b537ef23553bdf9cb707482b7b9ca040e814df2
Tony Messias <tonyzrp@gmail.com>
2017-05-05 17:51:49 -0300

    Translate all existing pages

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -1,8 +1,24 @@
 <?php
 
 return [
+    'fields' => [
+        'email' => 'E-mail Address',
+        'password' => 'Password',
+        'password_confirmation' => 'Confirmation Password',
+        'name' => 'Name',
+    ],
     'login' => [
-
+        'register' => 'Register',
+        'login_title' => 'Login',
+        'forgot_password_question' => 'Forgot your password?',
+        'forgot_password_title' => '',
+        'remember_me' => 'Remember me',
+        'login_btn' => 'Login',
+        'register_btn' => 'Register',
+        'register_title' => 'Register',
+        'reset_password_title' => 'Reset Password',
+        'reset_password_link_btn' => 'Send Password Reset Link',
+        'reset_password_btn' => 'Reset Password',
     ],
     'articles' => [
         'comments' => '{0} No comments yet|{1} :count comment|[2,*] :count comments',
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,15 +43,17 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;<li><a href="{{ route('home') }}">@lang('app.articles.nav_articles')</a></li>
+                        @if(auth()->check())
+                            <li><a href="{{ route('home') }}">@lang('app.articles.nav_articles')</a></li>
+                        @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}">@lang('app.login.login_btn')</a></li>
+                            <li><a href="{{ route('register') }}">@lang('app.login.register_btn')</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
